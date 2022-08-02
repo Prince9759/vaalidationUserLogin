c7a8e56b6180ccc60dd8f0b6b6647d0079101e8f
Prince9759 <prince.jain_cs18@gla.ac.in>
2022-08-02 17:23:46 +0530

    Create Welcome.java

---
+++ LoginManagement/src/main/java/com/businesslogic/Welcome.java
@@ -0,0 +1,35 @@
+package com.businesslogic;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/Welcome")
+public class Welcome extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+ 
+    public Welcome() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		PrintWriter out=response.getWriter();
+		out.println("<h1>Welcome User</h1>");
+	}
+
+}
